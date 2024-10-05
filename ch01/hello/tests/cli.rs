0553109c08fff0163ded460cf749a0146e39fec5
Tsuyoshi Yamamoto <tsuyoshi.yamamoto@jp.ricoh.com>
2024-10-05 10:58:38 +0900

    終了コード 1 を期待するテストを追加

--- ch01/hello/tests/cli.rs
+++ ch01/hello/tests/cli.rs
@@ -11,3 +11,9 @@ fn true_ok() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
     cmd.assert().success();
 }
+
+#[test]
+fn false_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
