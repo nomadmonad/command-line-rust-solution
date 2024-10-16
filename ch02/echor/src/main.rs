48966edb30918c7f4c24edd6bd12dd6019ed64ef
Tsuyoshi Yamamoto <tsuyoshi.yamamoto@jp.ricoh.com>
2024-10-16 23:49:28 +0900

    テスト用リソースを追加

--- ch02/echor/src/main.rs
+++ ch02/echor/src/main.rs
@@ -1,23 +1,53 @@
-use clap::Parser;
+use clap::{Arg, ArgAction, Command};
 
-#[derive(Parser, Debug)]
-#[command(name = "echor")]
-#[command(version = "0.1.0")]
-#[command(author = "Ken Youens-Clark <kyclark@gmail.com>")]
-#[command(about = "Rust echo")]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    text: String,
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
-}
+// #[derive(Parser, Debug)]
+// #[command(name = "echor")]
+// #[command(version = "0.1.0")]
+// #[command(author = "Ken Youens-Clark <kyclark@gmail.com>")]
+// #[command(about = "Rust echo")]
+// struct Args {
+//     /// Name of the person to greet
+//     #[arg(short, long)]
+//     text: String,
+//     /// Number of times to greet
+//     #[arg(short, long, default_value_t = 1)]
+//     count: u8,
+// }
 
 fn main() {
-    let args = Args::parse();
+    // let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {:?}", args.text);
-    }
+    // for _ in 0..args.count {
+    //     println!("Hello {:?}", args.text);
+    // }
+    let matches = Command::new("echor")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .action(ArgAction::Append)
+                .required(true),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .action(ArgAction::SetTrue)
+                .help("Do not print newline"),
+        )
+        .get_matches();
+    println!("{:#?}", matches);
+    let text: Vec<&str> = matches
+        .get_many("text")
+        .unwrap()
+        .map(String::as_str)
+        .collect();
+    let omit_new_line: bool = *matches.get_one("omit_newline").unwrap();
+    print!(
+        "{}{}",
+        text.join(" "),
+        if omit_new_line { "" } else { "\n" }
+    );
 }
